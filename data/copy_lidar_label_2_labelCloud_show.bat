2beee34f7b97513477fc71031d55ee6beb66a4e4
oneway3124 <190025935@qq.com>
2023-01-09 10:25:23 +0800

    增加两个脚本推送

---
+++ data/copy_lidar_label_2_labelCloud_show.bat
@@ -0,0 +1,3 @@
+copy D:\labelCloud-opt\data\kitti_2_sustech_example\lidar\000000.bin        D:\labelCloud-opt\pointclouds
+copy D:\labelCloud-opt\data\kitti_2_sustech_example\kitti_label\000000.txt  D:\labelCloud-opt\labels
+pause
--- kitti_2_sustech.py
+++ kitti_2_sustech.py
@@ -1,4 +1,4 @@
-
+# 该代码用于实现labelCloud的数据格式，转化为sustechpoints的格式
 # detection ./data/example/label ./data/example/calib ./data/example/tgt_label
 # calib 坐标系
 # camera 相机坐标系
@@ -11,28 +11,43 @@ import math
 import numpy as np
 import sys
 
-
+# 获得逆矩阵
+# v2c : Tr_velo_to_cam
+# rect: R0_rect
+# file: ./data/kitti_2_sustech_example/calib\\000000.txt
 def get_inv_matrix(file, v2c, rect):
     with open(file) as f:
+        # 读取来自kitti的数据集：000000.txt;
+        # list:8; P0,P1,P2,P3,R0_rect, Tr_velo
         lines = f.readlines()
+        # Tr_velo_to_cam: 6.92....
         trans = [x for x in filter(lambda s: s.startswith(v2c), lines)][0]
-
+        # 将字符串转化为浮点型的List
         matrix = [m for m in map(lambda x: float(x), trans.strip().split(" ")[1:])]
+        # 补齐成长度为16的list
         matrix = matrix + [0, 0, 0, 1]
+        # 将list转化为 array
         m = np.array(matrix)
+        # 然后转化为矩阵
         velo_to_cam = m.reshape([4, 4])
 
+        # R0_rect: 9.999
         trans = [x for x in filter(lambda s: s.startswith(rect), lines)][0]
+        # 将字符串转化为浮点型的List
         matrix = [m for m in map(lambda x: float(x), trans.strip().split(" ")[1:])]
+        # 将list转化为array，然后转化为3*3的矩阵
         m = np.array(matrix).reshape(3, 3)
 
         m = np.concatenate((m, np.expand_dims(np.zeros(3), 1)), axis=1)
-
+        #扩展为4*4,并且第4行，第4列为全0，第4行第4列为1
         rect = np.concatenate((m, np.expand_dims(np.array([0, 0, 0, 1]), 0)), axis=0)
 
-        print(velo_to_cam, rect)
+        print(velo_to_cam)
+        print('\n')
+        print(rect)
+        # rect * velo_to_cam
         m = np.matmul(rect, velo_to_cam)
-
+        # 求逆矩阵 (rect * velo_to_cam)^(-1)
         m = np.linalg.inv(m)
 
         return m
@@ -71,8 +86,6 @@ def trans_detection_label(src_label_path, src_calib_path, tgt_label_path):
         print(frame)
 
         inv_m = get_detection_inv_matrix(src_calib_path,frame)
-
-        print('xxx')
 
         with open(os.path.join(src_label_path,fname)) as f:
             lines = f.readlines()
